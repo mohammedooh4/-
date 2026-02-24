455bc35c7634313c5ab99431cecb90f9e17b42b6
mohammedooh4 <mohammedueu3@gmail.com>
2026-02-24 06:13:50 +0300

    Add files via upload

--- src/lib/firebase-config.ts
+++ src/lib/firebase-config.ts
@@ -8,7 +8,7 @@ const firebaseConfig = {
     storageBucket: "market-management-6129d.firebasestorage.app",
     messagingSenderId: "318547026343",
     appId: "1:318547026343:web:fc34f297cf750e852e1682",
-    measurementId: "G-SBGCMS9KT5",
+    measurementId: "G-SBGCMS9KT5"
 };
 
 // Initialize Firebase (only once)
